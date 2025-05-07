06227e48da7f5e99ca4545c0b7fb677781e39e57
shaafi56 <shaafiaadan12@gmail.com~>
2025-05-07 17:42:20 +0300

    i aded the code two category of crud one is view records and other is delete record

--- website/views.py
+++ website/views.py
@@ -47,3 +47,33 @@ def register_user(request):
         return render(request, 'register.html', {'form':form})
 
     return render(request, 'register.html', {'form':form})
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        #look up the record by primary key
+        customer_record = Record.objects.get(id=pk)
+        return render(request, 'record.html', {'customer_record': customer_record})
+    else:
+        messages.error(request, 'You must be logged in to view this page.')
+        return redirect('home')
+    
+# def delete_customer(request, pk):
+#     if request.user.is_authenticated:
+#         #look up the record by primary key
+#         delete_record = Record.objects.get(id=pk)
+#         delete_record.delete()
+#         messages.success(request, 'Record deleted successfully!')
+#         return redirect('home')
+#     else:
+#         messages.error(request, 'You must be logged in to delete a record.')
+#         return redirect('home')
+    
+def delete_record(request, pk):
+	if request.user.is_authenticated:
+		delete_it = Record.objects.get(id=pk)
+		delete_it.delete()
+		messages.success(request, "Record Deleted Successfully...")
+		return redirect('home')
+	else:
+		messages.success(request, "You Must Be Logged In To Do That...")
+		return redirect('home')
