5dd1fa1321bd093470f7ce1850ceeebbd58b91b4
shaafi56 <shaafiaadan12@gmail.com~>
2025-05-13 17:56:55 +0300

    updat record

--- website/views.py
+++ website/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 
 from website.models import Record
 from .forms import AddRecordForm, SignUpForm
+from django.http import JsonResponse
 
 # Create your views here.
 def home(request):
@@ -24,7 +25,6 @@ def home(request):
             return redirect('home')
     else:
         return render(request, 'home.html', {'form': form,'records': records})
-
 #logout user
 def logout_user(request):
     logout(request)
@@ -81,3 +81,17 @@ def add_record(request):
     else:
         messages.success(request, "You Must Be Logged In...")
         return redirect('home')
+
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk) 
+        form = AddRecordForm(request.POST or None, instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Record Has Been Updated!")
+            return redirect('home')
+        return render(request, 'update_record.html', {'form': form, 'current_record': current_record})
+    else:
+        messages.success(request, "You Must Be Logged In...")
+        return redirect('home')
+
